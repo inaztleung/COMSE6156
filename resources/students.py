93e5e6353211c20cba8b1b27e70c34a9b9660d34
inaztleung <zl3309@columbia.edu>
2023-09-18 00:05:08 -0400

    Updated

--- resources/students.py
+++ resources/students.py
@@ -8,7 +8,7 @@ class StudentsResource:
     #
 
     students_file = \
-        "/Users/Ina/PycharmProjects/e6156-microservice-1/resources/old-students.json"
+        "/Users/Ina/PycharmProjects/COMSE6156/resources/old-students.json"
 
     def __init__(self):
         self.students = None
